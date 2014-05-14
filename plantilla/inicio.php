47cfbb3396b4065c8625d37c1851d7152915271c
franrobles8 <franrobles8@gmail.com>
2014-05-14 19:48:54 +0200

    Creando la plantilla de inicio
    Creación en proceso de la plantilla de inicio con ayuda de iconos metro
    ui

---
+++ plantilla/inicio.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="es-ES">
+<head>
+	<meta charset="UTF-8">
+	<title>Base de Datos Robles Portillo</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="../js/jquery-2.1.0.js"></script>
+	<script type="text/javascript" src="../js/desplegable.js"></script>
+	<link href="../metro/min/iconFont.min.css" rel="stylesheet">
+</head>
+<body>
+	<?php include "../plantilla/sidebar.php" ?>
+	<div id="contenido">
+		<div id="contenido_thumbs">
+			<div class="fila_thumbs">
+				<div class="thumb">
+					<h2><i class="icon-clipboard-2"></i></h2>
+					<small>Avisos diarios</small>
+				</div>
+
+				<div class="thumb">
+					<h2><i class="icon-pencil"></i></h2>
+					<small>Nuevo Aviso</small>
+				</div>
+
+				<div class="thumb">
+					<h2><i class="icon-file"></i></h2>
+					<small>Presupuestos</small>
+				</div>
+
+				<div class="thumb">
+					<h2><i class="icon-clipboard-2"></i></h2>
+					<small>Avisos diarios</small>
+				</div>
+
+				<div class="thumb">
+					<h2><i class="icon-clipboard-2"></i></h2>
+					<small>Avisos diarios</small>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
