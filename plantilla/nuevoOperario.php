c8a263f84a82519969d33a14b92da7ee9c8bf4df
franrobles8 <franrobles8@gmail.com>
2014-05-21 22:22:59 +0200

    Formulario de insertar operario

--- plantilla/nuevoOperario.php
+++ plantilla/nuevoOperario.php
@@ -13,32 +13,71 @@
 <body>
 	<?php include "sidebar.php" ?>
 	<div id="contenido">
-		<div id="contenido_thumbs">
-			<div class="fila_thumbs">
-				<center>
+		<div id="contenedor_form_nuevo_aviso">
+			<div id="contenido_thumbs">
+				<div class="fila_thumbs">
 					<form action="../services/operarios/insertOperario.php" method="post" id="formNuevoEmpleado" name="formNuevoEmpleado">
-						<h2 style="font-size:60px; color: #aaa;"> Crear empleado </h2>
-						<div class="thumb">
-							<input type="text" id="idUsuario" name="idUsuario" placeholder="Identificador Usuario" required/> <br>
-							<input type="text" id="nombre" name="nombre" placeholder="Nombre" required/> <br>
-							<input type="text" id="apellido1" name="apellido1" placeholder="Primer apellido" required/> <br>
-							<input type="text" id="apellido2" name="apellido2" placeholder="Segundo apellido" required/> <br>
-							<input type="tel" id="telefono1" name="telefono1" placeholder="Telefono principal" maxlength="9" required onkeypress="return numeroTelefono(event);"/> <br>
-							<input type="tel" id="telefono2" name="telefono2" placeholder="Telefono secundario" maxlength="9" required onkeypress="return numeroTelefono(event);"/> <br>
+						<h2> Crear operario </h2>
+						<div id="form_col_izqda">
+							<div class="input_aviso">
+								<label for="idUsuario">Id Usuario: </label>
+								<input style="width: 100px;" type="text" id="idUsuario" name="idUsuario" placeholder="Id usuario" required/>
+							</div>
+							<div class="input_aviso">
+								<label for="nombre">Nombre: </label>
+								<input style="width: 200px;" type="text" id="nombre" name="nombre" placeholder="Nombre" required/>
+							</div>
+							<div class="input_aviso">
+								<label for="apellido1">Primer apellido: </label>
+								<input style="width: 200px;" type="text" id="apellido1" name="apellido1" placeholder="Primer apellido" required/> <br>
+							</div>
+							<div class="input_aviso">
+								<label for="apellido2">Segundo apellido: </label>
+								<input style="width: 200px;" type="text" id="apellido2" name="apellido2" placeholder="Segundo apellido" required/> <br>
+							</div>
+							<div class="input_aviso">
+								<label for="telefono1">Telefono principal: </label>
+								<input style="width: 200px;" type="tel" id="telefono1" name="telefono1" placeholder="Telefono principal" maxlength="9" required onkeypress="return numeroTelefono(event);"/>
+							</div>
+							<div class="input_aviso">
+								<label for="telefono2">Telefono secundario: </label>
+								<input style="width: 200px;" type="tel" id="telefono2" name="telefono2" placeholder="Telefono secundario" maxlength="9" required onkeypress="return numeroTelefono(event);"/>
+							</div>
+
 						</div>
-						<div class="thumb thumb2">
-							<input type="text" id="direccion" name="direccion" placeholder="Direccion" required/> <br>
-							<input type="text" id="dni" name="dni" placeholder="DNI" maxlength="9" required onBlur="validarDNI(document.getElementById('dni').value);"/> <br>
-							<input type="text" id="ss" name="ss" placeholder="Seg. Social" required/> <br>
-							<textarea id="observaciones" name="observaciones" placeholder="Observaciones" required no-rezize></textarea> <br>
-							<input type="text" id="foto" name="foto" placeholder="Foto" required/> <br>
-							<input type="date" id="fecha" name="fecha" placeholder="Fecha Contratacion" required/> <br>
+						<div id="form_col_dcha">
+							<div class="input_aviso">
+								<label for="direccion">Direccion: </label>
+								<input type="text" id="direccion" name="direccion" placeholder="Direccion" required/>
+							</div>
+							<div class="input_aviso">
+								<label for="dni">DNI: </label>
+								<input type="text" id="dni" name="dni" placeholder="DNI" maxlength="9" required onBlur="validarDNI(document.getElementById('dni').value);"/>
+							</div>
+							<div class="input_aviso">
+								<label for="ss">Nº Seguridad Social: </label>
+								<input type="text" id="ss" name="ss" placeholder="Seg. Social" required/>
+							</div>
+							<div class="input_aviso">
+								<label for="observaciones">Observaciones: </label>
+								<textarea id="observaciones" name="observaciones" placeholder="Observaciones" required no-rezize></textarea>
+							</div>
+							<div class="input_aviso">
+								<label for="foto">Foto: </label>
+								<input type="file" id="foto" name="foto" placeholder="Foto" required/>
+							</div>
+							<div class="input_aviso">
+								<label for="fecha">Fecha contratación: </label>
+								<input type="date" id="fecha" name="fecha" placeholder="Fecha Contratacion" required/>
+							</div>
+
+							
 						</div>
-						<div class="thumb thumb3">
-							<input type="submit" id="enviar" name="enviar" value="Crear empleado" onClick="validarDNI(document.getElementById('dni').value);"/>	
+						<div class="input_aviso">
+							<input style="text-align: center; float: left; margin-left: 320px;" onclick="validaTodo();" type="button" name="btn_introducir_aviso" id="enviar" value="Crear operario" onClick="validarDNI(document.getElementById('dni').value);">
 						</div>
 					</form>
-				</center>
+				</div>
 			</div>
 		</div>
 	</div>
