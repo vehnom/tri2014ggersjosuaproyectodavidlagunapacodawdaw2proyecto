b6e85b174ba5d2844fe156366006461c685bc874
elektro2014 <joshuaericgalvezmedina@gmail.com>
2014-05-21 23:50:40 +0200

    estilos arreglados
    estilos globales para usar en todos los formularios

--- plantilla/nuevoAviso.php
+++ plantilla/nuevoAviso.php
@@ -35,20 +35,22 @@
 <body>
 	<?php include "sidebar.php" ?>
 	<div id="contenido">
-		<div id="contenedor_form_nuevo_aviso">
-			<form id="form_nuevo_aviso" method="post" action="../services/introduceAviso.php">
+		<div id="contenedor_form">
+			<form class="form" id="form_nuevo_aviso" method="post" action="../services/introduceAviso.php">
 				<h2>Nuevo Aviso</h2>
 				<div id="form_col_izqda">
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="nota_aviso">Nota: </label>
 						<textarea style="width: 200px;" id="nota_aviso" name="nota_aviso" placeholder="ej: Se han quedado encerrados... "></textarea>
 						<div id="valida_nota_aviso"></div>
 					</div>
-					<div class="input_aviso">
+					
+					<div class="form_input">
 						<label for="hora_aviso">Hora: </label>
 						<input type="time" id="hora_aviso" name="hora_aviso">
 					</div>
-					<div class="input_aviso">
+					
+					<div class="form_input">
 						<label for="tipo_trabajo_aviso">Tipo de Trabajo: </label>
 						<select id="tipo_trabajo_aviso" name="tipo_trabajo_aviso" class="tipo_trabajo_aviso">
 							<option value="Cerrajeria">Cerrajeria</option>
@@ -56,7 +58,7 @@
 						</select>
 					</div>
 
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="citado_por_aviso">Citado por: </label>
 						<select id="citado_por_aviso" name="citado_por_aviso" class="citado_por_aviso">
 							<option value="Paco">Paco</option>
@@ -64,19 +66,19 @@
 						</select>
 					</div>
 
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="fecha_entrada_aviso">Fecha de entrada: </label>
 						<input type="datetime-local" id="fecha_entrada_aviso" name="fecha_entrada_aviso">
 					</div>
-
 				</div>
+				
 				<div id="form_col_dcha">
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="fecha_visita_aviso">Fecha de visita: </label>
 						<input type="datetime-local" id="fecha_visita_aviso" name="fecha_visita_aviso">
 					</div>
 
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="estado_aviso">Estado del aviso: </label>
 						<select id="estado_aviso" name="estado_aviso" class="estado_aviso">
 							<option value="1">Sigue</option>
@@ -84,7 +86,7 @@
 						</select>
 					</div>
 
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="nivel_reclamacion_aviso">Nivel de reclamación del aviso: </label>
 						<select id="nivel_reclamacion_aviso" name="nivel_reclamacion_aviso" class="nivel_reclamacion_aviso">
 							<option value="0">Normal</option>
@@ -93,7 +95,7 @@
 						</select>
 					</div>
 
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="procedencia_aviso">Procedencia del aviso: </label>
 						<select id="procedencia_aviso" name="procedencia_aviso" class="procedencia_aviso">
 							<option value="AXA">AXA</option>
@@ -102,12 +104,12 @@
 						</select>
 					</div>
 
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="poliza_aviso">Póliza: </label>
 						<input type="text" id="poliza_aviso" name="poliza_aviso">
 					</div>
 
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="requiere_profesional_aviso">Requiere profesional: </label>
 						<select id="requiere_profesional_aviso" name="requiere_profesional_aviso" class="requiere_profesional_aviso">
 							<option value="Si">Si</option>
@@ -115,20 +117,20 @@
 						</select>
 					</div>
 
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="importe_aviso">Importe (Euros): </label>
 						<input type="text" id="importe_aviso" name="importe_aviso" placeholder="ej: 58,65">
 					</div>
 
-					<div class="input_aviso">
+					<div class="form_input">
 						<label for="n_factura_provisinal_aviso">Nº Factura provisional: </label>
 						<input type="text" id="n_factura_provisinal_aviso" name="n_factura_provisinal_aviso">
 					</div>
 				</div>
-				<div class="input_aviso">
+				
+				<div class="form_input">
 					<input onclick="validaTodo();" type="button" name="btn_introducir_aviso" id="btn_introducir_aviso" value="Añadir">
 				</div>
-				
 			</form>
 		</div>
 	</div>
