0ef90593719b7559eb367daa30fea97d5d1fda19
David <vehnom13@gmail.com>
2014-06-19 15:25:38 +0200

    Cambiado de carpeta  SavisosMapa

--- services/avisos/SavisosMapa.php
+++ services/avisos/SavisosMapa.php
@@ -1,1 +1,11 @@
-SavisosMapa.php
+<?php
+include_once("../services/myBBDD.php");
+include("../../modelo/MAvisos.php");
+$mybd = new myBBDD();
+
+$array_avisos = getAvisos($mybd);
+$_SESSION['avisos'] = $array_avisos;
+print_r($_SESSION['avisos']);
+//header('Location: ../../plantilla/mapaAvisos.php');
+
+?>
