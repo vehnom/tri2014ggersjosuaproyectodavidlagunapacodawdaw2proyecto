bd74f556e9c9ae95e2dd330a8bd7ed72976c97be
elektro2014 <joshuaericgalvezmedina@gmail.com>
2014-06-19 17:38:28 +0200

    error al logarse
    tetris para cuando no metes bien los datos al login

--- plantilla/error_login.php
+++ plantilla/error_login.php
@@ -10,7 +10,24 @@
 	<link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
 	<script type="text/javascript" src="../js/jquery-2.1.0.js"></script>
 	<script type="text/javascript" src="../js/desplegable.js"></script>
-	<script type='text/javascript'>setTimeout('location.href = "../plantilla/index.php"',5000); </script>
+	<script type='text/javascript'>
+		setTimeout('location.href = "../plantilla/index.php"',5000);
+		
+		var myVar=setInterval(function(){cambiarValor()},1000);
+		
+		var restante = 5;
+		
+		function cambiarValor(){
+			var tiempo = document.getElementById('tiempo');
+		
+			tiempo.innerHTML = "";
+			
+			restante--;
+			
+			tiempo.innerHTML = restante;
+		}
+		
+	</script>
 </head>
 
 <div id="nombre_empresa">
@@ -18,7 +35,11 @@
 </div>
 <div id="contenedor_login">
 	<div id="inner_contenedor_login">
-		<div class="error_login">El usuario o la contrase&ntilde;a son incorrectos!<br>Redireccionando al login en 5 segundos...</div>
+		<div class="error_login">El usuario o la contrase&ntilde;a son incorrectos!<br>Redireccionando al login en <span id="tiempo">5</span> segundos...</div>
+		
+		<br><br><br>		
+		
+		<div class="error_login">Mientras tanto también puedes pulsar <a href="../tetris/tetris.html" target="_blank">aquí</a>.</div>
 	</div>
 
 </div>
