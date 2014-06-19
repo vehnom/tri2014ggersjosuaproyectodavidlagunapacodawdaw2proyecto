a2c80124599fdb54e3196ca254c64318810c6b7c
franrobles8 <franrobles8@gmail.com>
2014-06-19 17:05:09 +0200

    Nuevo aspecto de pedidos (misma programación)

--- js/pedidos.js
+++ js/pedidos.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		id = id.split("_")[1];
 		var nombre = this.parentNode.parentNode.childNodes[0].textContent;
 		var cantidad = $("#cantidad_"+id).val();
-		$("<div class='caja_producto' id='"+id+"'><span class='id_producto'>"+id+"</span><span class='nombre_producto'>"+nombre+"</span><span class='cantidad_producto'>"+cantidad+"</span></div>").appendTo($("#carrito_pedido"));
+		$("<div class='caja_producto' id='"+id+"'><span class='id_producto'><b>Id:</b> "+id+"</span><span class='nombre_producto'> <b>Nombre:</b> "+nombre+" </span><span class='cantidad_producto'> <b>Cantidad:</b> "+cantidad+"</span></div>").appendTo($("#carrito_pedido"));
 		var total_cantidad = $("#total_pedido input").eq(1).val();
 		total_cantidad = parseInt(total_cantidad) + parseInt(cantidad);
 		$("#total_pedido input").eq(1).val(total_cantidad);
--- plantilla/nuevoPedido.php
+++ plantilla/nuevoPedido.php
@@ -27,7 +27,7 @@
 						if($_SESSION['categorias'][$i]['Id_Categoria'] == $_SESSION['productos'][$e]["Id_Categoria"]){
 							$subir = 'subirbajarCantidad("'.$_SESSION['productos'][$e]['Id_Producto'].'","+");';
 							$bajar = 'subirbajarCantidad("'.$_SESSION['productos'][$e]['Id_Producto'].'","-");';
-							echo "<a class='contenedor_sub_pedido' href='#' id='".$_SESSION['productos'][$e]['Id_Producto']."'>".$_SESSION['productos'][$e]['Nombre']." <div class='cantidad'><button type='button' onclick='$bajar'>-</button><input type='text' value='0' id='cantidad_".$_SESSION['productos'][$e]['Id_Producto']."'/><button type='button' onclick='$subir'>+</button></div><div><button  class='btncomprar' id='btncomprar_".$_SESSION['productos'][$e]['Id_Producto']."' type='button'>Comprar</button></div></a>";
+							echo "<a class='contenedor_sub_pedido' href='#' id='".$_SESSION['productos'][$e]['Id_Producto']."'>".$_SESSION['productos'][$e]['Nombre']." <div class='cantidad'><button type='button' onclick='$bajar'>-</button><input type='text' value='0' id='cantidad_".$_SESSION['productos'][$e]['Id_Producto']."'/><button type='button' onclick='$subir'>+</button></div><div><button  class='btncomprar' id='btncomprar_".$_SESSION['productos'][$e]['Id_Producto']."' type='button'><i class='icon-cart-2 on-left'></i> Comprar</button></div></a>";
 						}			
 					}
 				echo "</div>";
@@ -45,16 +45,25 @@
 			</div>
 			<div id="btns_pedido">
 				<form class="form" id="form_nuevo_pedido" action="../services/pedidos/SinsertPedido.php" method="post">
-					<label>Hora: </label>
-					<input type="time" name="hora_productos" id="hora_productos"/>
-					<label>Seguimiento: </label>
-					<input type="text" name="seguimiento_productos" id="seguimiento_productos"/>
-					<label>Cantidad: </label>
-					<input type="text" name="cantidad_productos" id="cantidad_productos"/>
-					<input type="hidden" name="carrito_productos" id="carrito_productos" />
-					<button type="submit">Guardar</button>
+					<div>
+						<label>Hora: </label>
+						<input type="time" name="hora_productos" id="hora_productos"/>
+					</div>
+					<div>
+						<label>Seguimiento: </label>
+						<input type="text" name="seguimiento_productos" id="seguimiento_productos"/>
+					</div>
+					<div>
+						<label>Cantidad: </label>
+						<input type="text" name="cantidad_productos" id="cantidad_productos"/>
+					</div>
+					<div>
+						<input type="hidden" name="carrito_productos" id="carrito_productos" />
+						<button type="submit"><i class="icon-floppy on-left"></i> Guardar</button>
+						<button type="button" onclick="$('#carrito_pedido').html('');$('#total_pedido input').eq(1).val('0');"><i class="icon-remove on-left"></i> Limpiar</button>
+					</div>
 				</form>
-				<button type="button" onclick="$('#carrito_pedido').html('');$('#total_pedido input').eq(1).val('0');">Limpiar</button>
+				
 			</div>
 		</div>
 	</div>
